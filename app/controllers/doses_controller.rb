4a78e6a32549e1a6ef073abc838a02b940818ca2
Kimon Haars <kimon.haars@gmail.com>
2021-01-19 19:24:24 +0100

    I thought I had pushed Saturday's final changes. But apparently I had not. So here they come now.

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,34 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    @ingredient = Ingredient.find_by_name(params[:dose][:ingredient])
+    @dose.ingredient = @ingredient
+    # I am not yet checking whether it's a valid ingredient
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+
+end
